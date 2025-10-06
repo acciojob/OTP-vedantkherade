66c7da338e47b7d1802e82730756e5629632f985
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2025-10-06 10:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ const codes = document.querySelectorAll(".code");
 
 codes[0].focus();
 
-c
+codes.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (value.length === 1 && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = "";
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
